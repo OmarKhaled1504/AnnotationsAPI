4265fe744a4ca33a6dcd5cf411146105aad87ea9
OmarKhaled <omarkhaledkh21@gmail.com>
2025-05-30 15:44:30 +0300

    minor fixes in directory structure

--- Dtos/AnnotationDtos/AnnotationUpdateDto.cs
+++ Dtos/AnnotationDtos/AnnotationUpdateDto.cs
@@ -3,4 +3,4 @@ using System.ComponentModel.DataAnnotations;
 namespace ImageAnnotationAPI.Dtos;
 
 public record class AnnotationUpdateDto(
-    [Required][StringLength(40)]string AnnotationType);
+    [Required][StringLength(40)] string AnnotationType);
--- Program.cs
+++ Program.cs
@@ -99,7 +99,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
